22a9dd2cf0b4b0fbc0412e77bb18476aeac2ad5a
Yetunde-Yusuf <yetundeyusuf410@yahoo.com>
2024-03-11 22:39:53 +0000

    update

--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "node_count" {
   description = "Number of nodes in the Kubernetes cluster"
   type        = number
   # You can set a default value if needed
-  default     = 2
+  default = 2
 }
 
 variable "username" {
   description = "Username for the Kubernetes cluster nodes"
   type        = string
   # You can set a default value if needed
-  default     = "azureadmin"
+  default = "azureadmin"
 }
 variable "location" {
   type    = string 
